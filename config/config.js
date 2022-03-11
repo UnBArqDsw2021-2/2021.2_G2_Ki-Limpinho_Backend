5d4f0c8f2a3f519337ef5bd149be55c53f8e5f23
NilvanPeres <nilvanperes@gmail.com>
2022-03-11 18:32:55 -0300

    feat: add Admin env
    Co-authored-by:  Jonathan Jorge @Jonathan-Oliveira
    Co-authored-by: Henrique Melo @HenriqueAmorim20

--- config/config.js
+++ config/config.js
@@ -21,7 +21,9 @@ const envVarsSchema = Joi.object({
   MONGO_HOST: Joi.string().required()
     .description('Mongo DB host url'),
   MONGO_PORT: Joi.number()
-    .default(27017)
+    .default(27017),
+  EMAIL: Joi.string().required()
+  .description('Admin email'),
 }).unknown()
   .required();
 
@@ -38,7 +40,8 @@ const config = {
   mongo: {
     host: envVars.MONGO_HOST,
     port: envVars.MONGO_PORT
-  }
+  },
+  email: envVars.EMAIL,
 };
 
 module.exports = config;
